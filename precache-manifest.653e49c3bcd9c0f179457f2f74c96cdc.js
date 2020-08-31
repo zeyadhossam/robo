3e250419e101d9bb90fac5888b9e609aa6157cbd
zeyadhossam <51086000+zeyadhossam@users.noreply.github.com>
2020-08-31 03:12:56 +0200

    Updates

---
+++ precache-manifest.653e49c3bcd9c0f179457f2f74c96cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf3c0db28d48538e64607385ef0799a",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bc46d271b6a36abedbef",
+    "url": "/robo/static/css/main.6cfc6b15.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc46d271b6a36abedbef",
+    "url": "/robo/static/js/main.7afe8ac3.chunk.js"
+  },
+  {
+    "revision": "af839f1de40b3e92ec6c",
+    "url": "/robo/static/js/runtime-main.99a06c5d.js"
+  }
+]);
